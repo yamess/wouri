033f558999ee7538344eaacf7d185b9529499529
Boukari Yameogo <boukari.yameogo@kuliko.ai>
2024-08-04 20:24:11 -0400

    new

---
+++ worker/src/infrastructure/db/connection.rs
@@ -0,0 +1,14 @@
+use deadpool_redis::{Config, Pool, Runtime};
+
+pub fn establish_connection(connection_str: &str, pool_size: usize) -> Pool {
+    let cfg = Config::from_url(connection_str);
+    let pool= cfg
+        .builder()
+        .unwrap()
+        .max_size(pool_size)
+        .wait_timeout(Some(std::time::Duration::from_secs(30)))
+        .runtime(Runtime::Tokio1)
+        .build()
+        .expect("Failed to create pool");
+    pool
+}
